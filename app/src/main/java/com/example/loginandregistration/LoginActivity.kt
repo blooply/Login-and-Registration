49273e22cc257a211adca64036f7675e41487080
Daxton Brown <nugnuts.the.2nd@gmail.com>
2025-12-02 10:10:03 -0800

    Add constraint layout id

--- app/src/main/java/com/example/loginandregistration/LoginActivity.kt
+++ app/src/main/java/com/example/loginandregistration/LoginActivity.kt
@@ -16,7 +16,7 @@ class LoginActivity : AppCompatActivity() {
         enableEdgeToEdge()
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(binding.layoutLogin) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
