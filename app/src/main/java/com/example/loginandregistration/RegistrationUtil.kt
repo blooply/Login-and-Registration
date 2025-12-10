71eeeae6ab26d080005321a76ecd8e0844ee869d
Daxton Brown <nugnuts.the.2nd@gmail.com>
2025-12-10 08:37:33 -0800

    Do logic for registration

--- app/src/main/java/com/example/loginandregistration/RegistrationUtil.kt
+++ app/src/main/java/com/example/loginandregistration/RegistrationUtil.kt
@@ -5,7 +5,8 @@ package com.example.loginandregistration
 object RegistrationUtil {
     // use this in the test class for the is username taken test
     // make another similar list for some taken emails
-    var existingUsers = listOf("cosmicF", "cosmicY", "bob", "alice")
+    private var existingEmails = listOf("awidjw@gmail.com", "aoiwjd@yahoo.com")
+    private var existingUsers = listOf("cosmicF", "cosmicY", "itusi", "alice")
     // you can use listOf<type>() instead of making the list & adding individually
     // List<String> blah = new ArrayList<String>();
     // blah.add("hi")
@@ -16,7 +17,7 @@ object RegistrationUtil {
     // not already taken
     // minimum number of characters is 3
     fun validateUsername(username: String) : Boolean {
-        return true
+        return username.length > 2 && !existingUsers.contains(username)
     }
 
     // make sure meets security requirements (deprecated ones that are still used everywhere)
@@ -26,18 +27,18 @@ object RegistrationUtil {
     // both passwords match
     // not empty
     fun validatePassword(password : String, confirmPassword: String) : Boolean {
-        return true
+        return (password == confirmPassword && password.length > 7 && password.any{it.isUpperCase()} && password.any{it.isDigit()})
     }
 
     // isn't empty
     fun validateName(name: String) : Boolean {
-        return true
+        return name.isNotEmpty()
     }
 
     // isn't empty
     // make sure the email isn't used
     // make sure it's in the proper email format user@domain.tld
     fun validateEmail(email: String) : Boolean {
-        return true
+        return email != null && email.isNotEmpty() && !existingEmails.contains(email) && email.indexOf("@") > 0 && email.indexOf(".") > email.indexOf("@") + 1 && email.indexOf(".") == email.lastIndexOf(".") && email.indexOf(".") < email.length - 1
     }
 }
